b9a8a8f9cd098f840a2ef34f43d7ab6172cf442d
mhyrr <grolsen@gmail.com>
2024-12-03 00:14:29 -0500

    prod paths

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,10 @@ export default defineConfig(({ command }) => ({
         changeOrigin: true,
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true
   }
 })) 
