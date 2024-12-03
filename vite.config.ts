84b539dca3c17296415fe842b7c209da68dd62be
mhyrr <grolsen@gmail.com>
2024-12-03 15:44:55 -0500

    old config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     proxy: {
-      '/api': {
+      '/auth/token': {
         target: 'http://localhost:3000',
         changeOrigin: true,
       }
     }
   }
-})) 
+}) 
